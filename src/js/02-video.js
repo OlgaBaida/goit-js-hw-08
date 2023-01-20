ac66a9c4fae6a43e1e57c7133eaf59316e7ea46d
OlgaBaida <109526437+OlgaBaida@users.noreply.github.com>
2023-01-21 00:56:43 +0200

    work3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,7 @@ const CURRENT_TIME = "videoplayer-current-time";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1));
    
 function onPlay ({ seconds }) {
     localStorage.setItem(CURRENT_TIME, seconds)
